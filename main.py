a7dff52a15d76a25911c652d0919b394d7ed50e8
Brooks_Jason <brooj359@hsd.k12.or.us>
2023-11-29 08:51:57 -0800

    push 1

--- main.py
+++ main.py
@@ -7,33 +7,52 @@ name of the next card.
 from pypercard import App, Card
 
 
-def auto_func(app, card):
-    """
-    Called while transitioning from card 2, to card 3.
-    """
-    count = app.datastore.setdefault("counter", 0)
-    count += 1
-    app.datastore["counter"] = count
-    return "card3"
 
 
-# The templates for these cards can be found in pypercard.html.
-cards = [
-    Card("card1", auto_advance=10, transition="card2"),
-    Card("card2", auto_advance=20, transition=auto_func),
-    Card("card3", auto_advance=5, transition="card1"),
-]
 
 
 # Create the app while ensuring the counter is reset.
 carousel_app = App(
-    name="PyperCard carousel", datastore={"counter": 0}, cards=cards
+    name="PyperCard carousel", datastore={"counter": 0}, 
 )
 
 
-@carousel_app.transition("card2", "click", "reset")
-def reset(app, card):
+
+@carousel_app.transition("card1", "click", "go_to_2")
+def go_to_2(app, card):
+    return "card2"
+
+@carousel_app.transition("card1", "click", "go_to_3")
+def go_to_3(app, card):
+    return "card3"
+
+
+@carousel_app.transition("card2", "click", "go_to_1")
+def go_to_1(app, card):
     return "card1"
 
+@carousel_app.transition("card2", "click", "go_to_3")
+def go_to_3(app, card):
+    return "card3"
+
+@carousel_app.transition("card3", "click", "go_to_1")
+def go_to_1(app, card):
+    return "card1"
+
+@carousel_app.transition("card3", "click", "go_to_2")
+def go_to_3(app, card):
+    return "card2"
 
 carousel_app.start("card1")
+
+
+
+#ask for numbers from user
+
+# id("calc")
+# num1 = int(input("First number:"))
+# num2 = int(input("Second number:"))
+
+# answer = num1 + num2
+
+# print(answer)
